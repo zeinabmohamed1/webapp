86fd8b8a0a31fec022005c0f575dc0db36576ffa
zeinah <zeinahmohamed111@gmail.com>
2016-04-12 16:20:20 +0200

    add delete file &update file&update2 file

---
+++ delete.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$HostName="localhost";
+$db_name="webapp";
+$LoginName="root";
+$LoginPassword="";   
+?>
+<?php
+
+	$con = mysql_connect($HostName,$LoginName,$LoginPassword);
+	if (!$con)
+  	{
+  	die('Could not connect: ' . mysql_error());
+  	}
+  
+  	mysql_select_db($db_name , $con);
+	
+	$person_id= $_GET[person_id] ;
+	if ( isset($_GET['person_id'])) {
+	$sql = "DELETE FROM users WHERE user_id ='$u_id'" ;}
+	
+	mysql_query($sql,$con) ;
+	
+  mysql_close($con);
+  
+?>
+</body>
+</html>
---
+++ update2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$HostName="localhost";
+$db_name="webapp";
+$LoginName="root";
+$LoginPassword="";
+
+
+
+
+	$con = mysql_connect($HostName,$LoginName,$LoginPassword);
+	if (!$con)
+  	{
+  	die('Could not connect: ' . mysql_error());
+  	}
+  
+  	mysql_select_db($db_name , $con);
+	
+	$sql = "UPDATE users SET  user_name= '$_GET[u_name]',user_password='$_GET[u_password]' WHERE user_id= '$_GET[u_id]'" ;
+	
+	$result = mysql_query($sql,$con) ;
+	
+  mysql_close($con);
+  
+
+
+?>
+</body>
+</html>
