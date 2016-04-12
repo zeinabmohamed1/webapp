b1fa2cb3801c309636979c1e2adb3ff5362e63d6
zeinah <zeinahmohamed111@gmail.com>
2016-04-12 14:56:59 +0200

    modifieding index file

--- index.php
+++ index.php
@@ -6,5 +6,33 @@
 </head>
 
 <body>
+<form method="post">
+ name<input name="u_name" type="text" /><hr />
+ password<input name="u_pass" type="password" /><hr />
+ <input name="submit" type="submit" value="insert" />
+ </form>
+ 
+<?php 
+$HostName="localhost";
+$db_name="webapp";
+$LoginName="root";
+$LoginPassword="";
+
+
+$con = mysql_connect($HostName,$LoginName,$LoginPassword);
+mysql_query("set names 'utf8'");
+if (!$con){die('Could not connect: ' . mysql_error());}
+mysql_select_db($db_name , $con);
+if ( isset($_POST['u_name']) && isset($_POST['u_pass']) ) {
+$sql = "INSERT INTO users (user_name,user_password) VALUES ('$_POST[u_name]' ,'$_POST[u_pass]' )" ;
+} else {
+$sql = '';
+}
+
+mysql_query($sql,$con) ;
+
+mysql_close($con);
+
+?>
 </body>
 </html>
